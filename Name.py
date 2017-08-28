4b2090e9aad5f8023fe39bb81696e99dbd6cdef3
rsahukar <rahul.sahukar@gmail.com>
2017-08-28 11:30:57 +0530

    Removed unncessary files

--- Name.py
+++
@@ -1,8 +0,0 @@
-class Name:
-    fn=""
-    ln=""
-    def __init__(self,fname,lname):
-        self.fn = fname
-        self.ln = lname
-    def display(self):
-        print "%s, %s" % (self.fn,self.ln)
