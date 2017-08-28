165804d4ce2282940fd03e2c641d39f888bce7ae
rsahukar <rahul.sahukar@gmail.com>
2017-08-28 11:18:30 +0530

    Code changes

--- Math/utils.py
+++ Math/utils.py
@@ -1,4 +1,7 @@
-"""This is Math Module"""
+"""This is Math Module. It has the following operations
+   1. fact
+   2. fib
+"""
 def fact(val):
     """Gives Factorial of a number"""
     if(val==0 or val==1):
@@ -6,6 +9,7 @@ def fact(val):
     return val*fact(val-1);
 
 def fib(val):
+    """Gives nth fibonacci number"""
     if(val==0 or val==1):
          return 1;
     return fib(val-1) + fib(val-2);
