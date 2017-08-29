a0c6d373f0496be0df7b5f712ffd8eff7857a36b
rsahukar <rahul.sahukar@gmail.com>
2017-08-29 18:20:17 +0530

    added list dirs function

--- fileutils/fileutils.py
+++ fileutils/fileutils.py
@@ -1,8 +1,14 @@
+import os
+
 WORD_DELIM = " "
 
-
 def grep(search, filename, name=False, line=False):
-    matches = ""
+
+    """ This module is similar to grep in UNIX. It searches a string in a file and
+        returns all the lines that search string contains. We can specify filename
+        and linenumber if we want them in the grep results"""
+
+    matches = []
     count = 0
     file = filename+":" if name else ""
     lstr = ""
@@ -13,7 +19,24 @@ def grep(search, filename, name=False, line=False):
                 if line:
                     lstr = str(count)
                 misc = file + lstr + WORD_DELIM
-                matches += misc + l
+                matches.append(misc + l)
     fin.close()
-    print(matches)
     return matches
+
+def listfiles(path):
+    """ This module lists all the files under the given directory. It recursively goes
+        through all the directories under the given directory and returns a list"""
+    files = []
+    __list_dir__(path, files)
+    return files
+    
+def __list_dir__(path,files):
+    """ Don't call this function directory. Its acts as a support function for "listfiles"
+    """
+    l = os.listdir(path)
+    for i in l:
+        fp = os.path.join(path,i)
+        if(os.path.isdir(fp)):
+            __list_dir__(fp,files);
+        else:
+            files.append(fp)
